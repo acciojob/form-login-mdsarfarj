4be690d0e25b01a3779c6af6604c966f9389122f
Md Sarfraj <116196482+mdsarfarj@users.noreply.github.com>
2023-11-22 19:17:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 function getFormvalue() {
-    //Write your code here
+  var form = document.getElementById("form1");
 
+    // Access the input values using their names
+    var firstName = form.elements["fname"].value;
+    var lastName = form.elements["lname"].value;
+
+    // Display the values using alert
+    alert(firstName + " " + lastName);
+
+    // Prevent the default form submission
+    return false;
 }
